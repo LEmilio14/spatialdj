aca1bff38d6d18224c8d894f8b81bf6cbf39b1b3
LEmilio14 <47877642+LEmilio14@users.noreply.github.com>
2022-06-03 18:31:19 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+  redisHost: 'localhost',
+  redisPassword: 'your_password_for_redis_here',
+  sessionSecret: 'somesessionsecret',
+  passwordSaltRounds: 10,
+  youtube_key: 'youtube_api_key'
+}
